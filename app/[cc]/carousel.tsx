1828f0e81a4bd72f8dc3585702e86ec9a5dd9897
msrks <mshrrks@gmail.com>
2024-03-01 20:52:40 -0800

    Refactor carousel component styling

--- app/[cc]/carousel.tsx
+++ app/[cc]/carousel.tsx
@@ -18,7 +18,7 @@ import { homepageConfig } from "@/config/homepage";
 export default function CarouselComponent({ lang }: { lang: CC }) {
   return (
     <Carousel
-      className="w-full h-[calc(100vh-65px)] overflow-hidden px-8"
+      className="w-full overflow-hidden px-2"
       plugins={[
         Autoplay({
           delay: 5000,
@@ -29,26 +29,29 @@ export default function CarouselComponent({ lang }: { lang: CC }) {
         {homepageConfig.carouselItems.map((item, i) => (
           <CarouselItem key={i}>
             <Card>
-              <Link href={`${lang}/${item.href}`}>
-                <CardContent className="overflow-hidden relative flex items-center justify-center">
-                  <AspectRatio ratio={16 / 13} className="overflow-hidden">
-                    <div className="absolute inset-0 z-10 bg-black/20 flex justify-center items-center">
-                      <h1 className="text-xl md:text-3xl font-bold text-white text-center p-2 max-w-6xl">
+              <CardContent className="flex items-center justify-center">
+                <AspectRatio
+                  ratio={16 / 9}
+                  className="relative overflow-hidden"
+                >
+                  <div className="absolute inset-0 z-10 bg-black/20 flex justify-center items-center">
+                    <Link href={`${lang}/${item.href}`}>
+                      <h1 className="hover:underline text-xl md:text-3xl font-bold text-white text-center p-2 max-w-6xl">
                         {lang === "cn" ? item.titleCn : item.title}
                         <br />
                         <br />
                         {lang === "cn" ? item.descriptionCn : item.description}
                       </h1>
-                    </div>
-                    <Image
-                      src={item.imgSrc}
-                      alt=""
-                      fill
-                      className="object-cover bg-black/10"
-                    />
-                  </AspectRatio>
-                </CardContent>
-              </Link>
+                    </Link>
+                  </div>
+                  <Image
+                    src={item.imgSrc}
+                    alt=""
+                    fill
+                    className="object-cover bg-black/10 object-center"
+                  />
+                </AspectRatio>
+              </CardContent>
             </Card>
           </CarouselItem>
         ))}
