341e811b1a3acb792eacea038530e64bfda47e01
msrks <mshrrks@gmail.com>
2024-04-05 12:16:14 -0700

    add english descriptions

--- config/nature-school.ts
+++ config/nature-school.ts
@@ -14,20 +14,20 @@ export const natureSchoolConfig = {
       titleCn: "老市自然学校 团队",
       items: [
         {
-          name: "Tan Xiyun",
+          name: "Tan Xiyun Principal (Civilian Library Director,Old City Village Mayor) Civil Engineer for 10 years, with a wealth of construction. Hai Tou Primary School soccer coach, has many years of community governance experience, but also play music, love sports, good at all kinds of sports, swimming, soccer, volleyball, badminton ...... chess, love to read, love to think, heavy practice, love to travel, Beijing, Tibet, Japan cycling experience, in the United States in Silicon Valley as a soccer coach, is still in the Stanford University Tour",
           nameCn:
             "谭喜云 校長（平民書馆馆長,老市村村长）土木工程师10年，有丰富建筑。海头小学足球教练，有多年社区治理经验，同时也是玩音乐，爱运动，擅长各项体育运动，游泳，足球，排球，羽毛球……象棋，爱读书，爱思考，重实践，爱旅行，有北京，西藏，日本骑行经历，在美国硅谷当过足球教练，目前仍在斯坦福大学游学",
         },
         {
-          name: "Liang Baohuang",
+          name: "Leung Po Juan (Head of B&B)",
           nameCn: "梁宝璜 老師（民宿負責人）",
         },
         {
-          name: "Xue Dongfeng",
+          name: "Xue Dongfeng (Cuisine Expert)",
           nameCn: "薛冬风 老師（料理專家）",
         },
         {
-          name: "A Gui",
+          name: "Agui (expert in catching fish and shrimp)",
           nameCn: "阿贵老師（抓鱼摸虾专家）",
         },
       ],
@@ -37,16 +37,16 @@ export const natureSchoolConfig = {
       titleCn: "環境自然",
       items: [
         {
-          name: "Cafe",
           nameCn: "住在平民书馆与近水楼台民宿（自然融合建筑1）",
+          name: "Living in the Civilian Library and Near Water Pavilion Homestay",
         },
         {
-          name: "Salt Farm",
           nameCn: "玩在稻田埤塘邊 （下田體驗、埤塘划船、草地奔跑）",
+          name: "Playing on the edge of the rice field pond",
         },
         {
-          name: "3 Bridge Farm",
           nameCn: "結交朋友群體生活 （生活公約 訓練獨立 群體生活 培養自信）",
+          name: "Making friends group life",
         },
       ],
     },
@@ -55,117 +55,118 @@ export const natureSchoolConfig = {
       titleCn: "課程活潑",
       items: [
         {
-          name: "Cafe",
           nameCn:
             "從遊戲中學工程學 （不只是玩 從玩中累積知識 啟發物理、工程思維）",
+          name: "Learning engineering from games",
         },
         {
-          name: "Salt Farm",
           nameCn:
             "舞動身體與五感啟發 （學校沒有教的課 但卻讓你的是孩子一生最受用的啟發）",
+          name: "Inspiring the body and five senses",
         },
         {
-          name: "3 Bridge Farm",
           nameCn:
             "食農教育＋田野探索 （讓你的孩子留下最難忘的回憶 赤腳踩在田裡的感動）",
+          name: "Food and Farm Education",
         },
       ],
     },
   ],
   subsections2: [
     {
-      title: "Nature School Curriculum",
       titleCn: "合作師資",
+      title: "Cooperative Teachers",
       items: [],
     },
     {
-      title: "Student Camp",
       titleCn: "自然教育博物师资",
+      title: "Natural Education Museum Teacher",
       items: [
         {
-          name: "Cafe",
           nameCn:
             "海南博物界资深导师：卢刚 海口畓榃湿地研究所创始人，长期关注海南生态环境和自然环境变化发展。不管是地上长的，还是天上飞的，或是土里爬的，对于动植物卢老师会尽最大能力提供关于它们的自然常识、有趣知识或“冷知识”，为各位大小朋友做自然启蒙，引导他们走入大自然、热爱大自然",
+          name: "Senior mentor in Hainan Museum: Lu Gang",
         },
         {
-          name: "Salt Farm",
           nameCn:
             "松鼠学堂自然体验导师：刘艳 曾帮助老市村研发出针对青少年的自然教育课程，从植物、鸟类、昆虫、两栖爬行类动物，讲到湿地、滨海潮间带、热带雨林",
+          name: "Squirrel School Nature Experience Teacher: Liu Yan",
         },
         {
-          name: "3 Bridge Farm",
           nameCn:
             "現任立法委員暨知名繪本主編：陳培瑜 長年耕耘於孩子閱讀領域並曾創立兒童書店",
+          name: "Current legislator and well-known picture book editor: Chen Peiyu",
         },
       ],
     },
     {
-      title: "Boy Reporter Teacher",
       titleCn: "少年報導者師資",
+      title: "Boy Reporter Teacher",
       items: [
         {
-          name: "Cafe",
           nameCn:
             "少年報導者是由非營利媒體《報導者》針對兒少打造的深度新聞報導品牌，透過圖文、聲音、遊戲等形式，讓孩子認識國內外重要議題。 關於更多他們",
+          name: "Junior Reporter is a brand of in-depth news reporting for children and young adults created by the nonprofit media outlet The Reporter, which introduces children to important issues at home and abroad through graphics, audio, and games. More about them ",
         },
       ],
     },
     {
-      title: "Boy Reporter Teacher",
       titleCn: "「給高中生的」師資",
+      title: "Boy Reporter Teacher",
       items: [
         {
-          name: "Cafe",
           nameCn:
             "現任台灣疫苗發展協會理事長：何美鄉 臺灣流行疾病學專家、內科醫師/曾於SARS期間率先進入和平醫院，協助控制疫情。",
+          name: "Current Chairman of the Taiwan Vaccine Development Association: He Meixiang",
         },
         {
-          name: "Salt Farm",
           nameCn: "國立臺灣大學哲學博士：古秀鈴 任教於中國文化大學哲學系",
+          name: "Ph.D. in Philosophy from National Taiwan University: Gu Xiuling",
         },
         {
-          name: "",
           nameCn:
             "「華文朗讀節」策展人：胡川安 研究所於國立臺灣大學雙修考古學與歷史學/任教於國立中央大學中文系",
+          name: "Curator of the Chinese Reading Festival: Hu Chuanan",
         },
         {
-          name: "",
           nameCn:
             "現任臺北地院行政訴訟庭庭長兼法官：張瑜鳳  多重身分的章魚法官、斜槓作家",
+          name: "Current Chief Judge and Judge of the Administrative Litigation Division of the Taipei District Court: Zhang Yufeng",
         },
       ],
     },
   ],
   feedbackNavs: [
     {
-      title:
-        "Improvisational Music and Miyazaki Easter Egg in the Ancient Tamarind Forest",
       titleCn: "古树林里的即兴音乐与宫崎骏彩蛋丨走进儋州老市村",
+      title:
+        "Impromptu music and Miyazaki easter eggs in the ancient tree forest | Enter Danzhou Laoshi Village",
       href: "https://mp.weixin.qq.com/s/XtL56KlFbLEpuZwOmCVzxQ",
     },
     {
-      title:
-        "Haitonghui: How do we learn in the village and absorb energy from different cultures and nature.",
       titleCn: "海童会 : 我们如何在村庄学习，在不同的文化与大自然中吸取能量。",
+      title:
+        "Haitong Club: How we learn in the village and absorb energy in different cultures and nature.",
       href: "https://mp.weixin.qq.com/s/QOGtKsTm1BGtyj6_ITvOvA",
     },
     {
-      title: "Engineer Tan Xiyun: The old village and the new dream of",
       titleCn: "工程师谭喜云：一个海南返乡青年的老村新梦",
+      title:
+        "Engineer Tan Xiyun: The Old Village New Dream of a Hainan Returnee Youth",
       href: "https://mp.weixin.qq.com/s/u62YbK0tdJMeO88ZiJMfaw",
     },
   ],
   navs: [
     {
       imgSrc: "/tamarin2.jpeg",
-      title: "Mayor Class",
       titleCn: "市长课堂",
+      title: "Mayor Class",
       href: "/mayor-class",
     },
     {
       imgSrc: "/tamarin2.jpeg",
-      title: "Another Guys Class",
       titleCn: "另一个家伙的课堂",
+      title: "Another Guys Class",
       href: "/farm",
     },
     {
@@ -186,8 +187,8 @@ export const natureSchoolConfig = {
   activitiesNav: [
     {
       imgSrc: "/student-internship.png",
-      title: "Summer Internship for University Students",
       titleCn: "大學生暑假實習",
+      title: "Summer Internship for University Students",
       description:
         "The Old City Nature School is a rural community practicing environmental education and community development, and welcomes university students to come and exchange ideas. Especially during the period of June-August, the passionate summer in Hai Tou, Danzhou, can fill your thesis presentation with environmental education fervor.",
       descriptionCn:
@@ -195,8 +196,8 @@ export const natureSchoolConfig = {
     },
     {
       imgSrc: "/companyteambuilding.png",
-      title: "Company Team Building and Work Holidays",
       titleCn: "企业团建与工作假期",
+      title: "Company Team Building and Work Holidays",
       description:
         "Welcome companies and corporate groups to Laoshi Village for team building, annual meetings, and work holidays. Whether you visit Laoshi Village for relaxation and local customs or for the purpose of communication, you can experience the local customs of Laoshi Village, ancient trees, wetlands, oceans, and practical cases of grassroots community governance. Various interesting community building projects in education, culture, and psychology allow workplace professionals visiting Laoshi Village to have the opportunity to contact with nature and grassroots communities.",
       descriptionCn:
@@ -204,8 +205,8 @@ export const natureSchoolConfig = {
     },
     {
       imgSrc: "/crab.jpeg",
-      title: "Family Camp",
       titleCn: "沙灘露營",
+      title: "Beach Camping",
       description:
         "Watch this sunset around a campfire with a star-filled sky, sleep on the beach, stroll along the clean coastline, spend leisurely hours creating freely with seaweed, and go to sleep together gazing at the fire in anticipation of the sunrise!",
       descriptionCn:
@@ -213,82 +214,92 @@ export const natureSchoolConfig = {
     },
     {
       imgSrc: "/crab.jpeg",
-      title: "Another Guys Class",
       titleCn: "浮潛",
-      description: "Snorkeling",
+      title: "Snorkeling",
       descriptionCn:
         "來老市村也可以浮潛!跟當地魚夫借用的玻璃箱，即可淌水观察，也可以一潛而下遍地都是可愛的海膽，乾淨的海洋裡。海星、海兔、寄居蟹，豐富的潮間帶生物等你來探訪。",
+      description:
+        "You can also snorkel in Laoshi Village! With the glass box borrowed from the local fishermen, you can observe the water, or you can dive down to see the lovely sea urchins everywhere in the clean ocean. Starfish, sea rabbits, hermit crabs, rich intertidal creatures are waiting for you to explore.",
     },
     {
       imgSrc: "/crab.jpeg",
-      title: "Another Guys Class",
       titleCn: "撈魚 抓蝦",
-      description: "Fishing and shrimping",
+      title: "Fishing and shrimping",
       descriptionCn:
         "一人手上握一隻網子變身河川少年吧!老市村乾淨的河川可以讓大家輕易的觀察到螃蟹 蝦子 魚。是大人小孩都適合的全年活動。",
+      description:
+        "One person holds a net in his hand and transforms into a river",
     },
     {
       imgSrc: "/crab.jpeg",
-      title: "Another Guys Class",
       titleCn: "露營活動",
-      description: "Camping",
+      title: "Camping",
       descriptionCn:
         "老市自然學校，本身就是個露營場，提供帳棚出租、野炊料理出租，歡迎自駕行的朋友來訪。",
+      description:
+        "The Old City Nature School itself is a campsite, offering tent rentals and picnic cooking rentals, and welcomes friends who drive themselves to visit.",
     },
     {
       imgSrc: "/crab.jpeg",
-      title: "Another Guys Class",
       titleCn: "住宿 餐點",
-      description: "Camping",
+      title: "Accommodation and Meals",
       descriptionCn: "走訪老市村的訪客，歡迎來自然學校住宿一晚",
+      description:
+        "Visitors to Laoshi Village are welcome to stay at the Nature School for one night",
     },
     {
       imgSrc: "/crab.jpeg",
-      title: "Another Guys Class",
       titleCn: "參訪研学",
-      description: "Camping",
+      title: "Study Tour",
       descriptionCn:
         "提供對老市自然环境及乡土风情，社区发展项目有興趣者，一個實習交流參訪的平台!",
+      description:
+        "Provide a platform for those who are interested in the natural environment and local customs of Laoshi, and community development projects.",
     },
     {
       imgSrc: "/crab.jpeg",
-      title: "Another Guys Class",
       titleCn: "古法制盐DIY",
-      description: "Camping",
+      title: "Ancient Salt Making DIY",
       descriptionCn:
         "这边的海盐不以急速现代方式制作，以千年传统智慧制盐，在去除有害杂质之余，也保留了大量对人体有益的微量元素。村民煮玉米、煮粥，单单加老市盐下去味道即鲜美甘甜，成为一代老市人美好的回忆。，跟老市盐民一起做盐，享受好吃的海盐风味!",
+      description:
+        "The sea salt here is not made in a fast modern way, but in a thousand-year-old traditional way. In addition to removing harmful impurities, it also retains a large amount of beneficial trace elements for the human body. Villagers cook corn and porridge, and the taste is delicious and sweet just by adding Laoshi salt. It has become a good memory for a generation of Laoshi people. Make salt with the salt people of Laoshi and enjoy the delicious flavor of sea salt!",
     },
     {
       imgSrc: "/crab.jpeg",
-      title: "Another Guys Class",
       titleCn: "生态塘參觀",
-      description: "Camping",
+      title: "Ecological Pond Visit",
       descriptionCn:
         "生态塘里可以钓螃蟹也可以钓鱼，可以划船，还可以观鸟，每年3~7月中国最美小鸟栗喉蜂虎在这里筑巢繁殖。从污染性荒废的鱼塘改造成提高生态价值，提供栖息地及食物，人、鸟及其他生物各取所需，达致平衡。；老市村可持续发展与保育理念及落地实践可提供一个最好的自然教育与研究基地",
+      description:
+        "In the ecological pond, you can catch crabs and fish, row boats, and watch birds. From March to July every year, the most beautiful bird in China, the Rufous-throated Flycatcher, nests and breeds here. From a polluted and abandoned fish pond to a transformation that enhances ecological value, provides habitat and food, and meets the needs of humans, birds, and other organisms, achieving balance. The concept of sustainable development and conservation in Laoshi Village and the practical implementation can provide the best natural education and research base.",
     },
     {
       imgSrc: "/crab.jpeg",
-      title: "Another Guys Class",
       titleCn: "酸豆酱制作體驗",
-      description: "Camping",
+      title: "Soybean Sauce Making Experience",
       descriptionCn:
         "老市村是海南拥有最多酸豆树的乡村社区，用在地的食材，和傳統的道具，體驗老市村的酸豆酱吧!",
+      description:
+        "Laoshi Village is a rural community in Hainan with the most sour bean trees. Use local ingredients and traditional tools to experience the sour bean sauce of Laoshi Village!",
     },
     {
       imgSrc: "/crab.jpeg",
-      title: "Another Guys Class",
       titleCn: "乡村社区參訪",
-      description: "Camping",
+      title: "Village Community Visit",
       descriptionCn:
         "提供對社区治理參訪、實習有興趣的你一個交流的機會，可以到當地跟随村长實際事务擔任一天志工，跟著村民代表了解实际乡村运转与生活方式，美食，自然，民俗，社区发展等议题",
+      description:
+        "Provide you with an opportunity to visit and intern in community governance. You can follow the village chief to serve as a volunteer for a day, and follow the village representative to understand the actual operation and lifestyle of the village, food, nature, folklore, community development and other topics",
     },
     {
       imgSrc: "/crab.jpeg",
-      title: "Another Guys Class",
       titleCn: "地方镇小学交流活動",
-      description: "Camping",
+      title: "Local Town Primary School Exchange Activities",
       descriptionCn:
         "老市村自然學校與當地镇小学足球队有緊密的合作關係，歡迎中小学生與當地進行跨越城乡边界交流活動。可以透過一起上足球课，踢一场比赛拉近孩子之間距離，地方美食PARTY,或是海南本土地方會話讓孩子從小對地方文化多样性產生興趣。",
+      description:
+        "The Old City Nature School has a close cooperative relationship with the local town primary school football team, and welcomes primary and secondary school students to cross urban and rural boundaries for exchange activities with the local area. You can get closer to the children by taking football classes together, playing a game, local food PARTY, or local Hainanese conversations to make children interested in local cultural diversity from an early age.",
     },
   ],
 };
