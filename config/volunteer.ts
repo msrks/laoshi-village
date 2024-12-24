2dde0ae934bb14bfed7c345392df97832e940844
xiyuntan <160806967+xiyuntan@users.noreply.github.com>
2024-12-23 22:39:49 -0800

    Update volunteer.ts

--- config/volunteer.ts
+++ config/volunteer.ts
@@ -1,20 +1,20 @@
 export const volunteerConfig = {
   title: "Volunteer Recruitment",
-  titleCn: "義工招募aaabbbbbbb",
+  titleCn: "義工招募",
   imgSrc: "/volunteer/hero-volunteer.jpeg",
   description:
     "What is the eternal reliance of the hometown, we do not know, we only know that because of people, the hometown never withers. This is a sentence in the vision of the foundation, which reveals that volunteers in the foundation organization become the backbone of the structure through the transmission operation of the board of directors, the volunteer group, and the secretariat. The foundation's volunteer group has four types of organizations: the 'Wenjiao Small Volunteer' of junior high school students, the 'High School Volunteer' composed of high school students, the 'Xin Gangyi' from the college youth association, and the adult volunteer group. The four-stage organization is passed down in one vein, forming a close inheritance and public participation network. Through community participation, visit learning, and interactive fellowship, the volunteers of the foundation are always full of energy and motivation, ready to go, and sustainable. It is hoped that the people of Laoshi Village will increase their identification and public affairs participation in their hometown through the process of personal participation.",
   descriptionCn:
-    "什麼是故鄉永遠的依靠，我們不知道，我們只知道，因為人，故鄉永不凋謝。這是基金會願景中的一段話，道出義工在基金會組織中，透過董事會、義工團、秘書處的傳動運作成為架構中的骨幹。本會義工團有國中生的「文教小義工」、高中職學生組成的「高校義工」、來自大專青年聯誼會的「新港誼」、以及成人大義工，四種階段組織一脈相傳，形成緊密的傳承及公共參與網絡。透過社區參與、參訪學習、互動聯誼，使本會義工隨時充滿能量動力而蓄勢待發，永續不墜。期盼新港鄉親藉由親自參與的過程，彼此學習與合作，增加對故鄉的認同及公共事務參與力。",
+    "什么是故乡永远的依靠，我们不知道，我们只知道，因为人，故乡永不凋谢。希望义工在未来的社区发展基金组织中，透过董事会、志工团、秘书处的传动运作成为架构中的骨干。本会义工团有小初高中生的「小义工」、大学生组成的「高校义工」、来自青年会的「地方谊」、以及成人大义工，四种阶段组织一脉相传，形成紧密的传承及公共参与网络。透过社区参与、参访学习、互动联谊，使本会义工随时充满能量动力而蓄势待发，永续不坠。期盼海头乡亲藉由亲自参与的过程，彼此学习与合作，增加对故乡的认同及公共事务参与力。",
   navTitle: "Volunteer Group Introduction",
   navTitleCn: "義工分組介紹",
   navs: [
     {
       imgSrc: "/old-house-ts.jpg",
       title: "Volunteer Recruitment",
-      titleCn: "培桂堂",
+      titleCn: "艺文教育组",
       descriptionCn:
-        "對於藝術、文化、教育工作有興趣或經驗者，協助培桂堂古蹟維護、展場值勤、團體參觀之導覽解說，維持舊宅原始風貌並積極推動地方藝文活動，促進新港社區發展。",
+        "对于在地艺术、民俗文化、教育工作有兴趣或经验者，协助古迹维护，挖掘与激活、团体观光导览与解说，维持老房子原始风貌并积极推动地方艺文活动，促进海头镇社区发展。",
       description:
         "For those who are interested in or have experience in art, culture, and education work, assist in the maintenance of the Guigui Hall monument, exhibition duty, and group visit guidance, maintain the original appearance of the old house, and actively promote local art and cultural activities to promote the development of the Xingang community.",
       paramKey: "old-house-ts",
@@ -22,9 +22,9 @@ export const volunteerConfig = {
     {
       imgSrc: "/temple-ts.jpg",
       title: "Volunteer Recruitment",
-      titleCn: " gawegaaea",
+      titleCn: " 阅读写作推广组",
       descriptionCn:
-        "對wwwwwwwww於藝術、文化、教育工作有興趣或經驗者，協助培桂堂古蹟維護、展場值勤、團體參觀之導覽解說，維持舊宅原始風貌並積極推動地方藝文活動，促進新港社區發展。",
+        "对图书管理及阅读写作推广有兴趣及经验者，协助老市村建立及运营社区终身学习空间，推广读书写作风气。",
       description:
         "For those who are interested in or have experience in art, culture, and education work, assist in the maintenance of the Guigui Hall monument, exhibition duty, and group visit guidance, maintain the original appearance of the old house, and actively promote local art and cultural activities to promote the development of the Xingang community.",
       paramKey: "old-house-ts",
@@ -32,9 +32,9 @@ export const volunteerConfig = {
     {
       imgSrc: "/reading.png",
       title: "Volunteer Recruitment",
-      titleCn: "培桂堂",
+      titleCn: "美工编辑组",
       descriptionCn:
-        "對於藝術、文化、教育工作有興趣或經驗者，協助培桂堂古蹟維護、展場值勤、團體參觀之導覽解說，維持舊宅原始風貌並積極推動地方藝文活動，促進新港社區發展。",
+        "具编辑、采访、撰稿及美工设计基础或经验者，协助老市村刊物编辑及各项文宣品、海报及展览布置的设计。",
       description:
         "For those who are interested in or have experience in art, culture, and education work, assist in the maintenance of the Guigui Hall monument, exhibition duty, and group visit guidance, maintain the original appearance of the old house, and actively promote local art and cultural activities to promote the development of the Xingang community.",
       paramKey: "old-house-ts",
@@ -42,9 +42,9 @@ export const volunteerConfig = {
     {
       imgSrc: "/reading2.png",
       title: "Volunteer Recruitment",
-      titleCn: "培桂堂",
+      titleCn: "环保生态组",
       descriptionCn:
-        "對於藝術、文化、教育工作有興趣或經驗者，協助培桂堂古蹟維護、展場值勤、團體參觀之導覽解說，維持舊宅原始風貌並積極推動地方藝文活動，促進新港社區發展。",
+        "对于地方环境保护、自然生态、绿化美化工作有使命感的者，协助本村推动垃圾分类及减量、资源回收、环境绿美化及调查，记录老市自然生态等工作。",
       description:
         "For those who are interested in or have experience in art, culture, and education work, assist in the maintenance of the Guigui Hall monument, exhibition duty, and group visit guidance, maintain the original appearance of the old house, and actively promote local art and cultural activities to promote the development of the Xingang community.",
       paramKey: "old-house-ts",
