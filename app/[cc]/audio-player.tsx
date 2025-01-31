d045ffb859de3439ab0654a8feb9bdd99b787245
msrks <mshrrks@gmail.com>
2025-01-30 16:15:28 -0800

    feat: improve interaction handling for autoplay in audio player

--- app/[cc]/audio-player.tsx
+++ app/[cc]/audio-player.tsx
@@ -17,7 +17,11 @@ export function AudioPlayer() {
   useEffect(() => {
     const handleInteraction = () => setCanAutoPlay(true);
     window.addEventListener("click", handleInteraction, { once: true });
-    return () => window.removeEventListener("click", handleInteraction);
+    window.addEventListener("scroll", handleInteraction, { once: true });
+    return () => {
+      window.removeEventListener("click", handleInteraction);
+      window.removeEventListener("scroll", handleInteraction);
+    };
   }, [canAutoPlay]);
 
   useEffect(() => {
